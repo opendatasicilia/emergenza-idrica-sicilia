25e9f1ef887da2fe77150d5d51041f1bfe66847a
aborruso <aborruso@gmail.com>
2024-04-07 15:40:53 +0200

    update

--- docs/javascripts/varie.js
+++ docs/javascripts/varie.js
@@ -1,16 +1,24 @@
-// to open external links in new window
-function targetBlank() {
-  // remove subdomain of current site's url and setup regex
+// Funzione per aprire in una nuova scheda tutti i link esterni e i link ai file PDF
+function targetBlankForPDFs() {
+  // Rimuovi il sottodominio del sito corrente dall'URL e imposta un'espressione regolare per il confronto
   var internal = location.host.replace("www.", "");
-      internal = new RegExp(internal, "i");
+  internal = new RegExp(internal, "i");
 
-  var a = document.getElementsByTagName('a'); // then, grab every link on the page
+  // Prendi tutti i link presenti nella pagina
+  var a = document.getElementsByTagName('a');
   for (var i = 0; i < a.length; i++) {
-    var href = a[i].host; // set the host of each link
-    if( !internal.test(href) ) { // make sure the href doesn't contain current site's host
-      a[i].setAttribute('target', '_blank'); // if it doesn't, set attributes
+    var href = a[i].href; // Ottieni l'URL di ogni link
+
+    // Controlla se il link punta a un file PDF interno o se è esterno (o punta a un PDF)
+    if (internal.test(href) && href.endsWith('.pdf')) {
+      // Se il link è interno e punta a un PDF, imposta l'attributo target a _blank
+      a[i].setAttribute('target', '_blank');
+    } else if (!internal.test(a[i].hostname) || href.endsWith('.pdf')) {
+      // Se il link è esterno o punta a un PDF, imposta l'attributo target a _blank
+      a[i].setAttribute('target', '_blank');
     }
   }
 };
 
-targetBlank();
+// Esegui la funzione per applicare la regola ai link
+targetBlankForPDFs();
