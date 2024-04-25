cd058f40289a028eb3fd1663219ea2b2b449d2fb
aborruso <aborruso@gmail.com>
2024-04-25 10:10:29 +0200

    close #40

--- docs/javascripts/varie.js
+++ docs/javascripts/varie.js
@@ -1,21 +1,24 @@
 // Funzione per aprire in una nuova scheda tutti i link esterni e i link ai file PDF
 function targetBlankForPDFs() {
-  // Rimuovi il sottodominio del sito corrente dall'URL e imposta un'espressione regolare per il confronto
-  var internal = location.host.replace("www.", "");
-  internal = new RegExp(internal, "i");
+  // Ottieni l'host del sito corrente senza sottodomini
+  var internal = location.hostname.replace("www.", "");
+  var internalRegex = new RegExp(internal, "i");
+
+  // Aggiungi un'espressione regolare per riconoscere anche gli indirizzi IP locali
+  var localHost = /localhost|127\.0\.0\.1/;
 
   // Prendi tutti i link presenti nella pagina
-  var a = document.getElementsByTagName('a');
-  for (var i = 0; i < a.length; i++) {
-    var href = a[i].href; // Ottieni l'URL di ogni link
+  var links = document.getElementsByTagName('a');
+  for (var i = 0; i < links.length; i++) {
+    var href = links[i].href; // Ottieni l'URL di ogni link
 
-    // Controlla se il link punta a un file PDF interno o se è esterno (o punta a un PDF)
-    if (internal.test(href) && href.endsWith('.pdf')) {
-      // Se il link è interno e punta a un PDF, imposta l'attributo target a _blank
-      a[i].setAttribute('target', '_blank');
-    } else if (!internal.test(a[i].hostname) || href.endsWith('.pdf')) {
-      // Se il link è esterno o punta a un PDF, imposta l'attributo target a _blank
-      a[i].setAttribute('target', '_blank');
+    // Controlla se l'URL è considerato interno o esterno, e se punta a un file PDF
+    if ((internalRegex.test(links[i].hostname) || localHost.test(links[i].hostname)) && href.endsWith('.pdf')) {
+      // Se il link è interno (incluso localhost o IP locale) e punta a un PDF, apri in una nuova scheda
+      links[i].setAttribute('target', '_blank');
+    } else if (!internalRegex.test(links[i].hostname) && !localHost.test(links[i].hostname) || href.endsWith('.pdf')) {
+      // Se il link è esterno o punta a un PDF, apri in una nuova scheda
+      links[i].setAttribute('target', '_blank');
     }
   }
 };
