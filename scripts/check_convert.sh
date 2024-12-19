6b8caa7ec8e9173cad0f3fdf884268e087260bde
Dennis Angemi <dennisangemi@gmail.com>
2024-12-19 16:03:39 +0100

    [volumi invasati] update script estrazione e predisposizione action

--- scripts/check_convert.sh
+++ scripts/check_convert.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Questo script apre il sito della Regione Sicilia e controlla la presenza di nuovi documenti PDF contenenti
+# dati sui volumi invasati dalle dighe siciliane. Se trova nuovi PDF, li scarica e li converte in CSV sfruttando un llm. 
+
 # requirements
 # xq, scrape-cli, llm (con api key), mlr
 
+# constants
 URL="https://www.regione.sicilia.it/istituzioni/regione/strutture-regionali/presidenza-regione/autorita-bacino-distretto-idrografico-sicilia/siti-tematici/risorse-idriche/volumi-invasati-nelle-dighe-della-sicilia"
 PATH_PDFS_LIST="./risorse/pdfs_list.txt"
 URL_HOMEPAGE="https://www.regione.sicilia.it"
@@ -21,7 +25,7 @@ while read -r line; do
       echo "$line è un nuovo pdf"
    
       # converto il nome del pdf in volumi_YYYY-MM.pdf tramite ai llm
-      new_filename=$(echo "$line" | llm -s "converti il nome di questo file pdf nel formato volumi_YYYY-MM.pdf se si tratta di una tabella, oppure grafici_YYYY-MM.pdf se invece si tratta di grafici. Restituisci in output una sola riga" | tr -d '\n')
+      new_filename=$(echo "$line" | llm -m gemini-1.5-pro-latest -s "converti il nome di questo file pdf nel formato volumi_YYYY-MM.pdf se si tratta di una tabella, oppure grafici_YYYY-MM.pdf se invece si tratta di grafici. Restituisci in output una sola riga" | tr -d '\n')
       echo "il nuovo nome è $new_filename"
 
       # check if new_filename start with "volumi", in caso scarico ed estraggo csv. Se contiene "grafici" scarico e stop
@@ -37,7 +41,7 @@ while read -r line; do
          mkdir -p ./risorse/tmp
 
          # extract csv data
-         curl -skL "$URL_CSV_ANAGRAFICA_DIGHE" | llm -t volumi_dighe -a "./risorse/pdf/Tabelle/$new_filename" | sed -e :a -e '/^\n*$/{$d;N;ba' -e '}' > ./risorse/tmp/$new_filename.csv
+         curl -skL "$URL_CSV_ANAGRAFICA_DIGHE" | llm -m gemini-1.5-pro-latest -s "Il tuo compito è quello di estrarre dati da un pdf allegato e di incrociarli con i dati di un'anagrafica csv passata come prompt. Dalla tabella pdf, individua la data di rilevazione e poi estrai tutti i dati della colonna diga (chiamala 'diga_pdf') e quelli della colonna relativa al volume registrato nel mese più recente. Arricchisci la tabella aggiungendo una colonna chiamata 'data' che abbia in ogni riga la data della rilevazione più recente a cui si riferiscono i dati nel formato yyyy-mm-dd. Dal CSV, estrai la colonna 'diga' che chiamerai 'diga_anagrafica' popolata con il nome corretto (da includere esattamente nell'output). Confronta le colonne 'diga_pdf' e 'diga_anagrafica' per fare in modo di arricchire il dataset e assegnare a ogni diga il corrispondente codice identificativo presente nella colonna 'cod' del csv. Attenzione alla diga ogliastro e don sturzo che sono la stessa cosa. In ultimo, cestina la colonna 'diga_pdf' e  nell'output includi i valori di 'diga_anagrafica' sotto il nome di 'diga'. Attenzione ad attribuire correttamente il codice al nome della diga secondo l'anagrafica csv. Se l'anagrafica csv contiene più dighe della tabella pdf, l'output deve contenere solo ed esclusivamente le dighe presenti nel file pdf. Se nell'anagrafica ci sono più dighe del pdf, il volume non deve essere 0 ma deve essere vuoto. L'output deve avere questa struttura 'cod,diga,data,volume' e non deve avere righe vuote finali. Tieni presente che i valori di 'diga' devono essere esattamente coincidenti con quelli di 'diga_anagrafica'. I separatori di decimali dei volumi devono essere i punti e non le virgole. L'output non deve contenere i backtips all'inizio e alla fine ma solo il contenuto del file csv a cominciare dagli header L'output non deve contenere righe vuote alla fine. Se l'output csv contiene righe finali senza valori, rimuovile." -a "./risorse/pdf/Tabelle/$new_filename" | sed -e :a -e '/^\n*$/{$d;N;ba' -e '}' > ./risorse/tmp/$new_filename.csv
          echo "ho convertito $new_filename in $new_filename.csv"
 
          # attendo per eventuali limiti di api
@@ -64,7 +68,6 @@ else
    exit 0
 fi
 
-
 # if temp folder exists
 if [ -d "./risorse/tmp" ]; then
    # merge csv data (se ne esistono più di uno)
@@ -88,4 +91,3 @@ if [ -d "./risorse/tmp" ]; then
 fi
 
 echo "Fine, bye!"
-
